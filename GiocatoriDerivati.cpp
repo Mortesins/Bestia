5ed2ba87382659804bbdf8387a35c5f966040491
Mortesins <abernardinis@hotmail.com>
2018-08-30 18:04:24 +0200

    refactor GiocatoreUmano

--- GiocatoriDerivati.cpp
+++ GiocatoriDerivati.cpp
@@ -13,7 +13,7 @@ GiocatoreUmano::~GiocatoreUmano()
    carte[2] = NULL;
 }
 
-bool GiocatoreUmano::bussareOno() 
+bool GiocatoreUmano::bussareOno(const Carta* briscola) 
 {
    char a;
    cout << nome << ": bussare? (y/n) " << endl;
@@ -21,7 +21,7 @@ bool GiocatoreUmano::bussareOno()
    return ( (a == 'y') || (a == 'Y') ); 
 }
    
-bool GiocatoreUmano::vadoAlBuio() 
+bool GiocatoreUmano::vadoAlBuio(unsigned pot, bool nessunoAndato, bool ultimo, unsigned posto) 
 {
    char a;
    cout << nome << ": Andare al buio? (y/n) " << endl;
@@ -54,6 +54,7 @@ Carta* GiocatoreUmano::giocaCarta(unsigned diMano, const Carta* briscola, const 
    /*** ***/
    /*** COSA FARE ***/
    ///cout << "asso briscola: " << hoAssoBriscola << " briscole: " << briscole << endl;
+   unsigned a;
    bool sonoDiPrimaMano = true;
    //controllare di essere di prima
    for (unsigned i = 0; i < carteGiocate.size(); i++)
@@ -66,23 +67,14 @@ Carta* GiocatoreUmano::giocaCarta(unsigned diMano, const Carta* briscola, const 
       if (assoDiPrima && (hoAssoBriscola != -1)) // c'è la regola e ho l'asso
       {
          ///cout << "sono di mano e ho l'asso" << endl;
-         cout << nome << ": Quale carta giocare? (briscola: " << *briscola << " )" << endl;
-         for (unsigned i = 0; i < 3; i++)
+         a = scegliCartaDaGiocare(briscola);
+         while (a-1 != hoAssoBriscola)
          {
-            if (carte[i] != NULL)
-            {
-               cout << i << ") " << *(carte[i]) << endl;
-            }
-         }
-         int a;
-         cin >> a;
-         while (a != hoAssoBriscola)
-         {
-            cout << "Devi giocare l'asso!" << endl;
+            cout << "Devi giocare l'asso! Riscegliere la carta: ";
             cin >> a;
          }
-         Carta* c = carte[a];
-         carte[a] = NULL;
+         Carta* c = carte[a-1];
+         carte[a-1] = NULL;
          return c;
       }
       else if (secondaBriscola && briscole != 0) // c'è la regola ed ho briscola
@@ -99,34 +91,17 @@ Carta* GiocatoreUmano::giocaCarta(unsigned diMano, const Carta* briscola, const 
          if (carteVuote == 1) // sono di seconda
          {
             ///cout << "Sono di prima, ho briscola ed è secondo giro" << endl;
-            cout << nome << ": Quale carta giocare? Briscola: " << *briscola << endl;
-            for (unsigned i = 0; i < 3; i++)
-            {
-               if (carte[i] != NULL)
-               {
-                  cout << i << ") " << *(carte[i]) << endl;
-               }
-            }
-            unsigned a;
-            cin >> a;
-            bool briscolaGiocata = false;
-            if (a >= 0 && a < 3)
-            {
-               if (carte[a] != NULL)
-                  briscolaGiocata = (carte[a]->getSeme() == briscola->getSeme());
-            }
+            a = scegliCartaDaGiocare(briscola);
+            bool briscolaGiocata = (carte[a-1]->getSeme() == briscola->getSeme());
             while (!briscolaGiocata)
             {
-               cout << "Di seconda devi giocare briscola!" << endl;
+               cout << "Di seconda devi giocare briscola! Riscegliere la carta: ";
                cin >> a;
-               if (a >= 0 && a < 3)
-               {
-                  if (carte[a] != NULL)
-                     briscolaGiocata = (carte[a]->getSeme() == briscola->getSeme());
-               }
+               a = loopPerValoreValido(a);
+               briscolaGiocata = (carte[a-1]->getSeme() == briscola->getSeme());
             }
-            Carta* c = carte[a];
-            carte[a] = NULL;
+            Carta* c = carte[a-1];
+            carte[a-1] = NULL;
             return c;
          }
       }
@@ -134,23 +109,9 @@ Carta* GiocatoreUmano::giocaCarta(unsigned diMano, const Carta* briscola, const 
       ///cout << "sono di mano, gioco come voglio" << endl;
       //significa che sono io di mano
       // gioco come voglio
-      cout << nome << ": Quale carta giocare? Briscola: " << *briscola << endl;
-      for (unsigned i = 0; i < 3; i++)
-      {
-         if (carte[i] != NULL)
-         {
-            cout << i << ") " << *(carte[i]) << endl;
-         }
-      }
-      unsigned a;
-      cin >> a;
-      while (!(a == 0 || a == 1 || a == 2) || carte[a] == NULL )
-      {
-         cout << "Valore non valido. Riscegliere la carta: ";
-         cin >> a;
-      }
-      Carta* c = carte[a];
-      carte[a] = NULL;
+      a = scegliCartaDaGiocare(briscola);
+      Carta* c = carte[a-1];
+      carte[a-1] = NULL;
       return c;
    }
    /*** ***/
@@ -186,58 +147,27 @@ Carta* GiocatoreUmano::giocaCarta(unsigned diMano, const Carta* briscola, const 
       {
          // gioco briscola
          ///cout << "ho briscola" << endl;
-         cout << nome << ": Quale carta giocare? Briscola: " << *briscola << endl;
-         for (unsigned i = 0; i < 3; i++)
-         {
-            if (carte[i] != NULL)
-            {
-               cout << i << ") " << *(carte[i]) << endl;
-            }
-         }
-         unsigned a;
-         cin >> a;
-         bool briscolaGiocata = false;
-         if (a >= 0 && a < 3)
-         {
-            if (carte[a] != NULL)
-               briscolaGiocata = (carte[a]->getSeme() == briscola->getSeme());
-         }
+         a = scegliCartaDaGiocare(briscola);
+         bool briscolaGiocata = (carte[a-1]->getSeme() == briscola->getSeme());
          ///cout << "briscola giocata: " << briscolaGiocata << endl;
          while (!briscolaGiocata)
          {
-            cout << "Devi giocare briscola!" << endl;
+            cout << "Devi giocare briscola! Riscegliere la carta: ";
             cin >> a;
-            if (a >= 0 && a < 3)
-            {
-               if (carte[a] != NULL)
-                  briscolaGiocata = (carte[a]->getSeme() == briscola->getSeme());
-            }
+            a = loopPerValoreValido(a);
+            briscolaGiocata = (carte[a-1]->getSeme() == briscola->getSeme());
          }
-         Carta* c = carte[a];
-         carte[a] = NULL;
+         Carta* c = carte[a-1];
+         carte[a-1] = NULL;
          return c;
       }
       else
       {
          ///cout << "non ho ne briscola ne palo => gioco come voglio" << endl;
          //non ho ne briscola ne palo => gioco come voglio
-         cout << nome << ": Quale carta giocare? Briscola: " << *briscola << endl;
-         for (unsigned i = 0; i < 3; i++)
-         {
-            if (carte[i] != NULL)
-            {
-               cout << i << ") " << *(carte[i]) << endl;
-            }
-         }
-         unsigned a;
-         cin >> a;
-         while (!(a == 0 || a == 1 || a == 2) || carte[a] == NULL )
-         {
-            cout << "Valore non valido. Riscegliere la carta: ";
-            cin >> a;
-         }
-         Carta* c = carte[a];
-         carte[a] = NULL;
+         a = scegliCartaDaGiocare(briscola);
+         Carta* c = carte[a-1];
+         carte[a-1] = NULL;
          return c;
       }
    }
@@ -246,57 +176,50 @@ Carta* GiocatoreUmano::giocaCarta(unsigned diMano, const Carta* briscola, const 
       // ho palo
       // gioco palo
       ///cout << "ho palo" << endl;
-      cout << nome << ": Quale carta giocare? Briscola: " << *briscola << endl;
-      for (unsigned i = 0; i < 3; i++)
-      {
-         if (carte[i] != NULL)
-         {
-            cout << i << ") " << *(carte[i]) << endl;
-         }
-      }
-      unsigned a;
-      cin >> a;
-      bool paloGiocato = false;
-      if (a >= 0 && a < 3)
-      {
-         if (carte[a] != NULL)
-            paloGiocato = (carte[a]->getSeme() == palo);
-      }
+      a = scegliCartaDaGiocare(briscola);
+      bool paloGiocato = (carte[a-1]->getSeme() == palo);
       while (!paloGiocato)
       {
-         cout << "Devi seguire il seme!" << endl;
+         cout << "Devi seguire il seme! Riscegliere la carta: ";
          cin >> a;
-         if (a >= 0 && a < 3)
-         {
-            if (carte[a] != NULL)
-               paloGiocato = (carte[a]->getSeme() == palo);
-         }
+         a = loopPerValoreValido(a);
+         paloGiocato = (carte[a-1]->getSeme() == palo);
       }
-      Carta* c = carte[a];
-      carte[a] = NULL;
+      Carta* c = carte[a-1];
+      carte[a-1] = NULL;
       return c;
    }
    ///cout << "NIENTE" << endl;
+   a = scegliCartaDaGiocare(briscola);
+   Carta* c = carte[a-1];
+   carte[a-1] = NULL;
+   return c;
+}
+
+unsigned GiocatoreUmano::scegliCartaDaGiocare(const Carta* briscola)
+{
    cout << nome << ": Quale carta giocare? Briscola: " << *briscola << endl;
-   for (unsigned i = 0; i < 3; i++)
+   for (unsigned i = 1; i <= 3; i++)
    {
-      if (carte[i] != NULL)
+      if (carte[i-1] != NULL)
       {
-         cout << i << ") " << *(carte[i]) << endl;
+         cout << i << ") " << *(carte[i-1]) << endl;
       }
    }
    unsigned a;
    cin >> a;
-   while (!(a == 0 || a == 1 || a == 2) || carte[a] == NULL )
+   return loopPerValoreValido(a);
+}
+
+unsigned GiocatoreUmano::loopPerValoreValido(unsigned a)
+{
+   while (!(a == 1 || a == 2 || a == 3) || carte[a-1] == NULL)
    {
       cout << "Valore non valido. Riscegliere la carta: "; 
       cin >> a;
    }
-   Carta* c = carte[a];
-   carte[a] = NULL;
-   return c;
+   return a;   
 }
-
 
 /**** 
  * 
--- GiocatoriDerivati.hpp
+++ GiocatoriDerivati.hpp
@@ -11,16 +11,14 @@ class GiocatoreUmano: public Giocatore
   private:
    const bool assoDiPrima;
    const bool secondaBriscola;
+   unsigned scegliCartaDaGiocare(const Carta* briscola);
+   unsigned loopPerValoreValido(unsigned a);
   public:
    GiocatoreUmano(string name, bool assoPrima=true, bool briscolaSeconda=true);
    ~GiocatoreUmano();
 
-/* FOR TESTING */
-   bool bussareOno(); // OK
-   bool vadoAlBuio(); // OK
-/*-------------*/
-   bool bussareOno(const Carta* briscola) { return bussareOno(); };
-   bool vadoAlBuio(unsigned pot, bool nessunoAndato, bool ultimo, unsigned posto) { return vadoAlBuio(); };
+   bool bussareOno(const Carta* briscola);
+   bool vadoAlBuio(unsigned pot, bool nessunoAndato, bool ultimo, unsigned posto);
    Carta* giocaCarta(unsigned diMano, const Carta* briscola, const vector <Carta*> carteGiocate, bool ultimo, unsigned posto, bool debug=false); //FATTO MA NON SE SE FUNZIONA
       // debug non serve
    unsigned cartaPiuAlta();
